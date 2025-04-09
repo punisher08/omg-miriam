03ea37d1862de2b2d536030b7e325538b2c52351
Jhom <32788073+punisher08@users.noreply.github.com>
2025-04-09 13:04:38 +0800

    Update key-project-stats.php

--- key-project-stats.php
+++ key-project-stats.php
@@ -10,24 +10,24 @@
 
 // Create id attribute allowing for custom "anchor" value.
 $id = 'kpstats-' . $block['id'];
-if( !empty($block['anchor']) ) {
-    $id = $block['anchor'];
+if (!empty($block['anchor'])) {
+	$id = $block['anchor'];
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
 $className = 'quinbrook-featurepanels';
-if( !empty($block['className']) ) {
-    $className .= ' ' . $block['className'];
+if (!empty($block['className'])) {
+	$className .= ' ' . $block['className'];
 }
-if( !empty($block['align']) ) {
-    $className .= ' align' . $block['align'];
+if (!empty($block['align'])) {
+	$className .= ' align' . $block['align'];
 }
 
 $style = '';
-$section_bg = get_field( 'kp_stats_bg' );
+$section_bg = get_field('kp_stats_bg');
 
-if ( $section_bg ){
-    $style = "background-image: url(" . $section_bg . ");
+if ($section_bg) {
+	$style = "background-image: url(" . $section_bg . ");
                      background-repeat: no-repeat;
                      background-size: cover;
                      background-attachment: fixed;
@@ -40,36 +40,34 @@ $cta_button_text = get_field('kp_stats_button_text');
 $cta_button_link = get_field('kp_stats_button_link');
 
 ?>
-    <section id="<?php echo esc_attr( $id ); ?>" class="kpstats-panels" style="<?php echo $style; ?>">
-        <div class="container">
-        	<div class="row row-kps-title wow fadeInUp">
-        		<div class="col-12 text-center col-title">
-        			<h3><?php echo $intro; ?></h3>
-        		</div>
-        	</div>
-            <div class="row row-kps-panels">
-				<?php
-				while( have_rows('kp_stats_panels') ): the_row();
+<section id="<?php echo esc_attr($id); ?>" class="kpstats-panels" style="<?php echo $style; ?>">
+	<div class="container">
+		<div class="row row-kps-title wow fadeInUp">
+			<div class="col-12 text-center col-title">
+				<h3><?php echo $intro; ?></h3>
+			</div>
+		</div>
+		<div class="row row-kps-panels">
+			<?php
+			while (have_rows('kp_stats_panels')): the_row();
 
-					$cta_button_link = get_sub_field('')
-				?>
+			?>
 
 				<div class="kps-panels--stat wow fadeInUp">
-				    <div class="kps-icon"><img src="<?php the_sub_field('image'); ?>" alt="kps-icon"></div>
-				    <div class="kps-info">
-					    <div class="h3">
-					        <?php the_sub_field('number_prefix'); ?><span class="stat-number" data-n="<?php the_sub_field('animate_number'); ?>">0</span><?php the_sub_field('number_suffix'); ?>
-					    </div>
-					    <div class="btm"><?php echo get_sub_field('bottom'); ?></div>
+					<div class="kps-icon"><img src="<?php the_sub_field('image'); ?>" alt="kps-icon"></div>
+					<div class="kps-info">
+						<div class="h3">
+							<?php the_sub_field('number_prefix'); ?><span class="stat-number" data-n="<?php the_sub_field('animate_number'); ?>">0</span><?php the_sub_field('number_suffix'); ?>
+						</div>
+						<div class="btm"><?php echo get_sub_field('bottom'); ?></div>
 					</div>
 				</div>
-				<?php endwhile; ?>
-            </div>
-            <div class="row row-kps-cta wow fadeInUp">
-            	<a href="<?php echo $cta_button_link ? esc_url( $cta_button_link ) : '#'; ?>"
-					   class="btn mx-auto"><?php echo $cta_button_text; ?></a>
-            </div>
-        </div>
-        
-    </section>
-        
+			<?php endwhile; ?>
+		</div>
+		<div class="row row-kps-cta wow fadeInUp">
+			<a href="<?php echo $cta_button_link ? esc_url($cta_button_link) : '#'; ?>"
+				class="btn mx-auto"><?php echo $cta_button_text; ?></a>
+		</div>
+	</div>
+
+</section>
